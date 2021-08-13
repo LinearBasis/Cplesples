c2cf8471b165995dc3cf8efca079972bf0017710
Dacia Nicki <dnicki@il-e5.msk.21-school.ru>
2021-08-13 14:40:55 +0300

    some fixes

--- day03/ex03/DiamondTrap.hpp
+++ day03/ex03/DiamondTrap.hpp
@@ -15,32 +15,3 @@ public:
 	void	attack(const std::string& target);
 	void	whoAmI();
 };
-
-
-
-//		пути решения дикого костыля:
-//	1)	создать отдельные переменные внутри frag и scav, чтобы они были фактически разными
-//	3)	не сдавать этот проект
-DiamondTrap::DiamondTrap(std::string name)
-	: ClapTrap(name + "_clap_name"), FragTrap(name), ScavTrap(name)
-{
-	this->_name = name;
-	this->_hp = 100;		// Дичайший костыль во вселенной
-	this->_attack = 30;
-}
-
-DiamondTrap::~DiamondTrap()
-{
-
-}
-
-
-void	DiamondTrap::attack(const std::string& target)
-{
-	ScavTrap::attack(target);
-}
-void	DiamondTrap::whoAmI()
-{
-	std::cout << "I am " << this->_name << std::endl;
-	std::cout << "Claptrap is " << ClapTrap::_name << std::endl;
-}
