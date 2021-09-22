b2c5d9363ee64692967f0ba622ca509f864db8b7
Dacia Nicki <dnicki@mi-i1.msk.21-school.ru>
2021-09-22 15:25:12 +0300

    fixed 07 iter

--- day07/ex01/iter.hpp
+++ day07/ex01/iter.hpp
@@ -2,13 +2,13 @@
 #include <iostream>
 
 template <class T>
-void	iter(T* arr, size_t len, T (*f)(T))
+void	iter(T* arr, size_t len, void (*f)(T))
 {
 	if (!arr)
 		throw std::invalid_argument("nullptr array");
 	for (size_t i = 0; i < len; i++)
 	{
-		std::cout << f(arr[i]) << " ";
+		f(arr[i]);
 	}
 	std::cout << std::endl;
 }
--- day07/ex01/main.cpp
+++ day07/ex01/main.cpp
@@ -1,9 +1,38 @@
 #include "iter.hpp"
-#include <cmath>
 
-int		main()
+void myFunc(int a)
 {
-	float	a[] = {1, 4, 9, 16, 25, 36};
-
-	iter(a, 6, std::sqrt);
+    int res = a * a;
+    std::cout << "Res :" << res << std::endl;
+    // return (res);
 }
+
+int main( void )
+{
+    int arr[] = {0, 1, 2, 3, 4, 5, 6};
+    iter(arr, 7, myFunc);
+}
+
+// class Awesome
+// {
+//     public:
+//     Awesome( void ) : _n( 42 ) { return; }
+//     int get( void ) const { return this->_n; }
+//     private:
+//     int _n;
+// };
+
+// std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
+
+
+
+// int main() 
+// {
+//     int tab[] = { 0, 1, 2, 3, 4 }; // <--- I never understood why you can't write int[] tab. Wouldn't that make more sense?
+//     Awesome tab2[5];
+
+//     iter(tab, 5, print);
+//     iter(tab2, 5, print);
+
+//     return 0;
+// }
